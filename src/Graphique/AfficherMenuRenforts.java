e917039f7de164c20d273c75b748f0b804a2d86f
LaitNoisette <meyerfranckpro@gmail.com>
2018-06-08 21:24:06 +0200

    Dernier commentaire

--- src/Graphique/AfficherMenuRenforts.java
+++ src/Graphique/AfficherMenuRenforts.java
@@ -146,6 +146,7 @@ public class AfficherMenuRenforts extends FXMLDocumentController{
 				System.out.println("fedlsk");
 				// SALUT FRANCK
 				FXMLDocumentController.batailleJeu.jouerBataille();
+				//ajouter changement couleur territoire
 				//FXMLDocumentController.batailleJeu=null;
 			}		
 		});
